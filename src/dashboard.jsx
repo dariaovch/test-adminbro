37edbe712613ba2b662cfe4d98274287fb4d5591
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-12-07 20:34:35 +0100

    added chart

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -3,26 +3,41 @@
 import React from 'react';
 import { ApiClient } from 'admin-bro';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Box } from '@admin-bro/design-system';
+import { Label, Box } from '@admin-bro/design-system';
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 const api = new ApiClient();
 
+// Создаем компонент для Дашборда, получаем в него данные ресурса "Погода" и строим график
+
 export default function Dashboard() {
-  const [data, setData] = React.useState({});
+  const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    api.getDashboard()
-      .then((res) => {
-        setData(res.data);
-        console.log(data);
+    api.resourceAction({ resourceId: 'weather', actionName: 'list' })
+      .then((results) => {
+        const { records } = results.data;
+        setData(records);
       });
   }, []);
 
+  const chartData = data.map((item) => ({ name: <item className="params id"></item>, temp: item.params.temperature }));
+
   return (
-    <Box variant="grey">
-      <Box variant="white">
-        {data.sky}
-        {console.log(data)}
+    <Box variant='grey'>
+      <Label>Temperature changes</Label>
+      <Box variant='white'>
+        <LineChart width={400} height={400} data={chartData}>
+            <Line type="monotone" dataKey="temp" stroke="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+          </LineChart>
       </Box>
     </Box>
   );
